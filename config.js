4ba157543163d3e768c408750001441ff8da1955
Umesh Singh <umeshsingh0202@gmail.com>
2026-08-17 15:43:02 +0530

    Update Google Sheets script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Sheets Web App Endpoint Configuration
 // Update your Google Apps Script URL here anytime without touching index.html
-const GOOGLE_SHEETS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGiY4iWaTpx7S1vj0Nf74J4FZKB1VAxbtPhSD8Pmd7DKTk5zVQyMFsyNPPpr3OFXo-/exec';
+const GOOGLE_SHEETS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxJfsfbYpLAPejBWeoFasmpN4jlPMQUo27iBPafSGbkpa_bDEVxzmODfYEODeb3ZngG/exec';
